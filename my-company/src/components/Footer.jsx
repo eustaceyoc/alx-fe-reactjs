396076575d233208c9841b22c0b71ca50b1b2f54
Eustace Crentsil <crentsil.eustace25@gmail.com>
2026-01-18 23:20:24 +0000

    Build a simple company website with React, routing, Navbar, Footer, and Contact form

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      © 2023 Our Company
+    </footer>
+  );
+}
+
+export default Footer;
