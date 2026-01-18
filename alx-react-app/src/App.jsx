2fd5eeff1869a793660787ab815ed8e17acb7e5e
Eustace Crentsil <crentsil.eustace25@gmail.com>
2026-01-18 19:56:56 +0000

    Task 3.

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
-    return (
-        <div>
-            <WelcomeMessage />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
