9921a7f38c2dc3a1b4e31b8e54b8a64482581990
Eustace Crentsil <crentsil.eustace25@gmail.com>
2026-01-23 15:21:17 +0000

    Add setRecipes action to recipe store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,14 @@ import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
+
   addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+    })),
+
+  setRecipes: (recipes) =>
+    set(() => ({
+      recipes: recipes,
+    })),
 }));
