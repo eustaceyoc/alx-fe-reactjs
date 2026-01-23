1eec392fb569e90aa4106e225e6bf76f8ac3999c
Eustace Crentsil <crentsil.eustace25@gmail.com>
2026-01-23 15:32:35 +0000

    Add recipe details, edit and delete functionality

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
 import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
 
 function RecipeList() {
   const recipes = useRecipeStore((state) => state.recipes);
@@ -9,8 +10,9 @@ function RecipeList() {
 
       {recipes.map((recipe) => (
         <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
+          <Link to={`/recipe/${recipe.id}`}>
+            <h3>{recipe.title}</h3>
+          </Link>
         </div>
       ))}
     </div>
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -8,8 +8,17 @@ export const useRecipeStore = create((set) => ({
       recipes: [...state.recipes, newRecipe],
     })),
 
-  setRecipes: (recipes) =>
-    set(() => ({
-      recipes: recipes,
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
+
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  setRecipes: (recipes) => set({ recipes }),
 }));
