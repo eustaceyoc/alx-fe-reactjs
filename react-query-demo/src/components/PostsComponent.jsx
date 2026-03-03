c0b1d3757a9c837c794ff5b8277ea7582f3248c7
Eustace Crentsil <crentsil.eustace25@gmail.com>
2026-03-03 15:25:43 +0000

    Add react-query-demo

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,48 @@
+import { useQuery } from "react-query";
+
+const fetchPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!res.ok) throw new Error("Failed to fetch");
+  return res.json();
+};
+
+function PostsComponent() {
+  const {
+    data,
+    error,
+    isLoading,
+    isError,
+    refetch,
+    isFetching,
+  } = useQuery("posts", fetchPosts, {
+    staleTime: 1000 * 60 * 2, 
+    cacheTime: 1000 * 60 * 10, 
+    refetchOnWindowFocus: false,
+    keepPreviousData: true,
+  });
+
+  if (isLoading) return <p>Loading posts...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h2>Posts</h2>
+
+      <button onClick={() => refetch()}>
+        Refetch Posts
+      </button>
+
+      {isFetching && <p>Updating data...</p>}
+
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <strong>{post.title}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostsComponent;
